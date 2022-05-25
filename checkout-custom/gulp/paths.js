5af45dfd3e89c191c6c4ecc9e10720ff18cdadc2
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-05-25 11:26:22 -0300

    feat: gulp tasks

---
+++ checkout-custom/gulp/paths.js
@@ -0,0 +1,57 @@
+// All default supported file extensions are declared here!
+export const Extensions = Object.freeze({
+  html: `html,htm`,
+  scripts: `js,ts`,
+  styles: `css,scss`,
+  assets: `jpeg,jpg,png`,
+});
+
+// All default folders are declared here!
+export const Folders = Object.freeze({
+  dist: `./dist`,
+  src: {
+    root: `./src`,
+    styles: `./src/styles`,
+    assets: `./src/assets`,
+  },
+});
+
+// All default globs are declared here!
+export const Globs = Object.freeze({
+  src: {
+    build: {
+      scripts: [
+        `${Folders.src.root}/*.{${Extensions.scripts}}`,
+        `!./**/*.test.{${Extensions.scripts}}`,
+      ],
+      styles: [`${Folders.src.styles}/*.{${Extensions.styles}}`],
+      assets: [`${Folders.src.assets.assets}/*.{${Extensions.assets}}`],
+      scripts_modules: [
+        `${Folders.src.root}/**/*.{${Extensions.scripts}}`,
+        `!${Folders.src.root}/*.{${Extensions.scripts}}`,
+        `!./**/*.test.{${Extensions.scripts}}`,
+      ],
+      styles_modules: [
+        `${Folders.src.styles}/**/*.{${Extensions.styles}}`,
+        `!${Folders.src.styles}/*.{${Extensions.styles}}`,
+      ],
+    },
+    linters: {
+      scripts: [
+        `${Folders.src.root}/*.{${Extensions.scripts}}`,
+        `${Folders.src.root}/**/*.{${Extensions.scripts}}`,
+        `!./**/*.test.{${Extensions.scripts}}`,
+      ],
+      styles: [
+        `${Folders.src.styles}/*.{${Extensions.styles}}`,
+        `${Folders.src.styles}/**/*.{${Extensions.styles}}`,
+      ],
+    },
+  },
+  views: {
+    html: [
+      `./src/views/*.{${Extensions.html}}`,
+      `./src/views/**/*.{${Extensions.html}}`,
+    ],
+  },
+});
---
+++ checkout-custom/gulpfile.babel.js
@@ -0,0 +1,33 @@
+/**
+ * Econverse Build
+ * https://econverse.digital/
+ * Author: Vitor Seles <vitor.seles@econverse.com.br>
+ * Repository / Docs: <https://github.com/vseles/econverse-build>
+ */
+
+import { series } from 'gulp';
+import * as Tasks from './gulp/tasks';
+
+// Declare watch task;
+exports.watch = Tasks.watcher;
+
+// Declare clear task;
+exports.clear = Tasks.clear;
+
+// Declare HTMLHINT task;
+exports.htmlhint = Tasks.htmlhint;
+
+// Declare stylelint task;
+exports.stylelint = Tasks.stylelint;
+
+// Declare eslint task;
+exports.eslint = Tasks.eslint;
+
+// Declare Prettier task;
+exports.prettier = Tasks.prettier;
+
+// Declare Images task;
+exports.images = Tasks.images;
+
+// Declare build task as series;
+exports.build = series(Tasks.clear, Tasks.scripts, Tasks.styles);
